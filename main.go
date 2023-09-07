b00a1758aec02dc26eeeaec6c5c6352df1fe8fb1
JerryAgbesi <jerryeagbesi@gmail.com>
2023-09-07 17:20:36 +0000

    Change method for fixing cors error

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"time"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,15 +14,15 @@ func main() {
 	router := gin.Default()
 	router.GET("api/", getJson)
 
-	config := cors.DefaultConfig()
-    config.AllowAllOrigins = true
-    config.AllowMethods = []string{"POST", "GET", "PUT", "OPTIONS"}
-    config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization", "Accept", "User-Agent", "Cache-Control", "Pragma"}
-    config.ExposeHeaders = []string{"Content-Length"}
-    config.AllowCredentials = true
-    config.MaxAge = 12 * time.Hour
+	// config := cors.DefaultConfig()
+    // config.AllowAllOrigins = true
+    // config.AllowMethods = []string{"POST", "GET", "PUT", "OPTIONS"}
+    // config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization", "Accept", "User-Agent", "Cache-Control", "Pragma"}
+    // config.ExposeHeaders = []string{"Content-Length"}
+    // config.AllowCredentials = true
+    // config.MaxAge = 12 * time.Hour
 
-    router.Use(cors.New(config))
+    router.Use(CORS())
 
 	port := os.Getenv("PORT")
 
